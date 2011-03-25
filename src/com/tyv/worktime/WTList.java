8186e52e30cce67d1bb05d8ac03083b9ae13df04
yaroslav.tarasov@gmail.com <yaroslav.tarasov@gmail.com@1abf2a90-cdb8-6873-103f-8c5fd7dc9d3a>
2011-03-25 18:29:36 +0000

    Еще одно неправильно работающее Activity

--- src/com/tyv/worktime/WTList.java
+++ src/com/tyv/worktime/WTList.java
@@ -1,5 +1,10 @@
 package com.tyv.worktime;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.database.Cursor;
@@ -7,14 +12,21 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.SimpleCursorAdapter.CursorToStringConverter;
 
 
 
 
-public class WTList extends ListActivity {
+public class WTList extends Activity { // Áûë ListActivity
     private static final String TAG = "WTList";
+    
+    private ArrayList<TimePoint> tpItems;
+    private ListView myListView;
+    private EditText myEditText;   
+    private TimePointAdapter aa;
 
     // Menu item ids
     public static final int MENU_ITEM_DELETE = Menu.FIRST;
@@ -35,7 +47,9 @@ public class WTList extends ListActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        
+        setContentView(R.layout.main2);
+        
         setDefaultKeyMode(DEFAULT_KEYS_SHORTCUT);
 
         // If no data was given in the intent (because we were started
@@ -60,33 +74,57 @@ public class WTList extends ListActivity {
         Uri mUri = intent.getData();
         
         // Inform the list we provide context menus for items
-        getListView().setOnCreateContextMenuListener(this);
+//        getListView().setOnCreateContextMenuListener(this);
         
         // Perform a managed query. The Activity will handle closing and requerying the cursor
         // when needed.
         Cursor cursor = managedQuery(getIntent().getData(), PROJECTION, null, null,
         		WorktimeProvider.DEFAULT_SORT_ORDER);
+
+//        // Used to map notes entries from the database to views
+//        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, R.layout.wtlist_item, cursor,
+//                new String[] { WorktimeProvider.KEY_DATE, WorktimeProvider.KEY_DETAILS}, new int[] { android.R.id.text1,android.R.id.text2 });
+//        
+//        CursorToStringConverter converter = new CursorToStringConverter() {
+//
+//            @Override
+//            public CharSequence convertToString(Cursor cursor) {
+//               int desiredColumn = 1;
+//               long date = cursor.getLong(desiredColumn);
+//	       	   SimpleDateFormat sdf = new SimpleDateFormat("d-MMMM-yyyy  HH.mm");
+//	    	   String dateString = sdf.format(date);	      
+//	    	    
+//               return dateString;
+//            }
+//         };   
+//         
+//         adapter.setCursorToStringConverter(converter);    
+//        
+//        setListAdapter(adapter);
         
+        myListView = (ListView)findViewById(R.id.myListView);
+        myEditText = (EditText)findViewById(R.id.myEditText);
 
+        tpItems = new ArrayList<TimePoint>();
+        int resID = R.layout.tplist_item;
+        aa = new TimePointAdapter(this, resID, tpItems);
+        myListView.setAdapter(aa);        
+      
+      //cursor.requery();
 
+	  //tpItems.clear();
+	    
+	  if (cursor.moveToFirst())
+	    do { 
+	      String task = cursor.getString(cursor.getColumnIndex(WorktimeProvider.KEY_DETAILS));
+	      long created = cursor.getLong(cursor.getColumnIndex(WorktimeProvider.KEY_DATE));
 
-
-        // Used to map notes entries from the database to views
-        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, R.layout.wtlist_item, cursor,
-                new String[] { WorktimeProvider.KEY_DATE, WorktimeProvider.KEY_DETAILS}, new int[] { android.R.id.text1,android.R.id.text2 });
+	      TimePoint newItem = new TimePoint( new Date(created),task);
+	      tpItems.add(0, newItem);
+	    } while(cursor.moveToNext());
+	  
+	   aa.notifyDataSetChanged();       
         
-        CursorToStringConverter converter = new CursorToStringConverter() {
-
-            @Override
-            public CharSequence convertToString(Cursor cursor) {
-               int desiredColumn = 1;
-               return cursor.getString(desiredColumn);
-            }
-         };   
-         
-         adapter.setCursorToStringConverter(converter);    
-        
-        setListAdapter(adapter);
     }
     
 }
